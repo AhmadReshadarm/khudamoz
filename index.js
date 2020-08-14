4ca7f3114738363b71f95d0adef1a3a15d153e40
AhmadReshadarm <armreshad@gmail.com>
2020-08-14 19:46:42 +0300

    first commit for khud amoz project

---
+++ index.js
@@ -0,0 +1,18 @@
+const themBtn = document.querySelector(".loader-wapper");
+const btnOutter = document.getElementById("them-mode-outer");
+const btnInner = document.getElementById("them-mode-inner");
+const overLayer = document.getElementById("overLayer");
+themBtn.addEventListener("click", ()=>{
+    btnOutter.classList.toggle("loader-light");
+    btnInner.classList.toggle("loader-moon");
+    overLayer.classList.add("overLayer");
+    setTimeout(()=>{
+        btnOutter.style.border = "2px dotted #222831";
+        btnInner.classList.remove("sun");
+        btnInner.classList.remove("loader-moon");
+        btnInner.classList.add("moon")
+    }, 350);
+    setTimeout(()=>{
+        overLayer.classList.remove("overLayer");
+    },1000)
+})
