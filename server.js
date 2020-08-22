3c2a156a0ff9841727bb83f14cd1bd59f01f5e0d
Rishad Mohammadi <armreshad@gmail.com>
2020-08-22 23:31:41 +0300

    still fixed the herku problem with adding res.sendFile option

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static(__dirname));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname), "./public", "index.html");
+});
+
 app.listen(3000, () => {
   console.log("sever is running on port 3000");
 });
