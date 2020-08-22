246978154977597f27295902d6a9e5e37c77c3c6
Rishad Mohammadi <armreshad@gmail.com>
2020-08-22 23:57:21 +0300

    fixed heroku not running problem with adding prosses.env.PORT to server.js file

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("sever is running on port 3000");
 });
