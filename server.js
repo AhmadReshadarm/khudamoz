b7d45cc52cfa19573721a3fb1992a8c3fb40038f
Rishad Mohammadi <armreshad@gmail.com>
2020-12-13 13:06:34 +0300

    final commit onpoint test project

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/dark.css");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("sever is running on port 3000");
+app.listen(process.env.PORT || 3001, () => {
+  console.log("sever is running on port 3001");
 });
